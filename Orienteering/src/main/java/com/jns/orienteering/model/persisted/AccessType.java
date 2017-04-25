4a71f9b86e92f418c69ab05491546257d4405da3
jkstraw <jkstraw@jkstraw-PC>
2017-04-26 00:36:12 +0200

    add copyright

--- Orienteering/src/main/java/com/jns/orienteering/model/persisted/AccessType.java
+++ Orienteering/src/main/java/com/jns/orienteering/model/persisted/AccessType.java
@@ -1,6 +1,6 @@
-/**
+/*
  *
- *  Copyright (c) 2016, Jens Stroh
+ *  Copyright 2016 - 2017, Jens Stroh
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
